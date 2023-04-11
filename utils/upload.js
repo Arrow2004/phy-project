83294666ffab070c2f54f3857114981154c55f3d
Arrow2004 <Arrow2004hacker@gmail.com>
2023-04-11 14:52:00 +0500

    upload-error-attempt-7

--- utils/upload.js
+++ utils/upload.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 //Set storage
 const storage = multer.diskStorage({
-    destination: '/public/uploads',
+    destination: './public/uploads',
     filename: function(req,file,cb){
         cb(null, file.fieldname+'-'+Date.now()+path.extname(file.originalname))
     }
