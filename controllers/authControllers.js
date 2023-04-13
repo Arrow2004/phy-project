51a0f333c9dc9b3d4465378f6187b56ccf5405e8
Arrow2004 <Arrow2004hacker@gmail.com>
2023-04-13 15:57:32 +0500

    some bug fixes

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,24 +2,33 @@ const User =require('../models/userModel')
 const uploadImage = require('../utils/cloudinary')
 const getLoginPage =(req,res)=>{
     res.render('user/login',{
-        title: 'Hisobga kirish'
+        title: 'Hisobga kirish',
+        regError: req.flash('loginError')
     })
 }
 const getRegisterPage =(req,res)=>{
     res.render('user/signup',{
-        title: 'Ro\'yxatdan o\'tish'
+        title: 'Ro\'yxatdan o\'tish',
+        regError: req.flash('regError')
     })
 }
 const login = async (req,res)=>{
     try {
         const userExist = await User.findOne({email: req.body.email})
-        if(userExist && userExist.password===req.body.password){
-            req.session.user = userExist;
-            req.session.isLogged = true;
-            res.redirect('/profile/'+userExist.username)
+        if(userExist){
+            if(userExist.password===req.body.password){
+                req.session.user = userExist;
+                req.session.isLogged = true;
+                res.redirect('/profile/'+userExist.username)
+            }else{
+                req.flash('loginError',`Login yoki parol xato!!!`)
+                res.redirect('/auth/login')
+            }
         }else{
-            res.redirect('/auth/signup')
+            req.flash('loginError',`Bunday foydalanuvchi topilmadi!!!`)
+                res.redirect('/auth/login')
         }
+        
     } catch (e) {
         console.log(e)
     }
@@ -28,19 +37,25 @@ const signup = async (req,res)=>{
     try {
         const userExist = await User.findOne({email: req.body.email})
         const uploadPic = await uploadImage(req.file)
-        if(!userExist && req.body.password==req.body.password2){
-            await User.create({
-                email: req.body.email,
-                profilePic: uploadPic,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                phone: req.body.phone,
-                password: req.body.password,
-                username: req.body.username,
-            })
-            res.redirect('/auth/login')
+        if(!userExist){
+            if(req.body.password==req.body.password2){
+                await User.create({
+                    email: req.body.email,
+                    profilePic: uploadPic,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    phone: req.body.phone,
+                    password: req.body.password,
+                    username: req.body.username,
+                })
+                res.redirect('/auth/login')
+            }else{
+                req.flash('regError','Qandaydur xatolik yuz berdi');
+                res.redirect('/auth/signup')
+            }
         }else{
-            res.redirect('/auth/signup')
+            req.flash('regError','Bunday foydalanuvchi mavjud');
+            return res.redirect('/auth/signup')
         }
     } catch (e) {
         console.log(e)
